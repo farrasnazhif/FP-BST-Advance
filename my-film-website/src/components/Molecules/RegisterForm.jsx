ac68ac46bf936cc4d46ce48f62d59573d5c4c5c6
farrasnazhif <farrasnazhif@gmail.com>
2024-09-19 14:55:12 +0700

    Update RegisterForm.jsx
    penyimpanan data akun pada local storage yang dapat digunakan pada halaman login

--- my-film-website/src/components/Molecules/RegisterForm.jsx
+++ my-film-website/src/components/Molecules/RegisterForm.jsx
@@ -1,36 +1,55 @@
+import React from "react";
 import Button from "../Atoms/Button";
 import InputForm from "../Atoms/FormInput";
 
+const handleRegister = (event) => {
+  event.preventDefault();
+  const username = event.target.username.value;
+  const email = event.target.email.value;
+  const password = event.target.password.value;
+  const confirmPassword = event.target.confirmPassword.value;
+
+  if (password !== confirmPassword) {
+    alert("Passwords do not match.");
+    return;
+  }
+
+  localStorage.setItem('username', username);
+  localStorage.setItem('email', email);
+  localStorage.setItem('password', password);
+
+  window.location.href = "/login";
+};
+
 const FormRegister = () => {
   return (
-    <form action="">
-      <InputForm 
-      label="fullname" 
-      type="text" 
-      placeholder="Insert your fullname" 
-      name="fullname"
+    <form onSubmit={handleRegister}>
+      <InputForm
+        label="username"
+        type="text"
+        placeholder="Insert your username"
+        name="username"
       />
-      <InputForm 
-      label="email" 
-      type="email" 
-      placeholder="example@mail.come" 
-      name="email"
+      <InputForm
+        label="email"
+        type="email"
+        placeholder="example@mail.com"
+        name="email"
       />
-      <InputForm l
-      label="password" 
-      type="password" 
-      placeholder="********" 
-      name="password"
+      <InputForm
+        label="password"
+        type="password"
+        placeholder="********"
+        name="password"
       />
-      <InputForm l
-      label="confirm password" 
-      type="password" 
-      placeholder="********" 
-      name="confirmPassword"
+      <InputForm
+        label="confirm password"
+        type="password"
+        placeholder="********"
+        name="confirmPassword"
       />
-      <Button classname="bg-cyan-500 w-full type=submit">Register</Button>
+      <Button classname="bg-cyan-500 w-full" type="submit">Register</Button>
     </form>
-
   );
 };
 
