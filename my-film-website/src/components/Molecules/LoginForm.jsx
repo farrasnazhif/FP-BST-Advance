97378d910c08bd648b5a14ec6671359fe7f66358
farrasnazhif <farrasnazhif@gmail.com>
2024-09-19 15:59:38 +0700

    Update LoginForm.jsx
    update router

--- my-film-website/src/components/Molecules/LoginForm.jsx
+++ my-film-website/src/components/Molecules/LoginForm.jsx
@@ -11,7 +11,7 @@ const handleLogin = (event) => {
   const storedPassword = localStorage.getItem('password');
 
   if (username === storedUsername && password === storedPassword) {
-    window.location.href = "/";
+    window.location.href = "/movies";
   } else {
     alert("Invalid username or password.");
   }
