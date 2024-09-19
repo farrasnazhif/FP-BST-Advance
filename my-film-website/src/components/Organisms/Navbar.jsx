c11fae57982e968be2f8cac76276dd9bf9f4c533
farrasnazhif <farrasnazhif@gmail.com>
2024-09-19 15:50:19 +0700

    Update Navbar.jsx
    update navbar into children component

--- my-film-website/src/components/Organisms/Navbar.jsx
+++ my-film-website/src/components/Organisms/Navbar.jsx
@@ -1,67 +1,26 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { MdMenu, MdAccountCircle } from "react-icons/md";
+import { MdMenu } from "react-icons/md";
 
-const NavbarMenu = [
-  {
-    id: 1,
-    name: "Home",
-    link: "/",
-  },
-  {
-    id: 2,
-    name: "Tv Shows",
-    link: "#list",
-  },
-  {
-    id: 3,
-    name: "Movies",
-    link: "#list",
-  },
-  {
-    id: 4,
-    name: "Upcoming",
-    link: "#",
-  },
-];
-
-const Navbar = () => {
+const Navbar = ({ children }) => {
   const [menuOpen, setMenuOpen] = useState(false); // State to toggle the menu
 
   return (
     <>
       <div className="bg-brandDark text-white py-6">
         <nav className="container flex justify-between items-center">
-          {/* logo section */}
+          {/* Logo section */}
           <div>
-            <a href="#" className="text-2xl font-bold uppercase">
+            <a href="/movies" className="text-2xl font-bold uppercase">
               28 <span className="font-extralight">CINEMA</span>
             </a>
           </div>
-          {/* menu section */}
+          {/* Menu section for desktop */}
           <div className="hidden md:block">
-            <ul className="flex items-center gap-4 ">
-              {NavbarMenu.map((menu) => (
-                <li
-                  key={menu.id}
-                  className="hover:text-slate-300
-                    transition duration-300 ease-in-out transform hover:scale-105
-                    text-xs py-2 px-3 uppercase"
-                >
-                  <a href={menu.link}>{menu.name}</a>
-                </li>
-              ))}
-              <button
-                className="hover:text-slate-300
-                      transition duration-300 ease-in-out transform hover:scale-105 text-xl ps-14"
-              >
-                <Link to="/login">
-                  <MdAccountCircle />
-                </Link>
-              </button>
+            <ul className="flex items-center gap-4">
+              {children}
             </ul>
           </div>
-          {/* mobile hamburger */}
+          {/* Mobile hamburger */}
           <div className="md:hidden">
             <button onClick={() => setMenuOpen(!menuOpen)}>
               <MdMenu />
@@ -73,21 +32,7 @@ const Navbar = () => {
         {menuOpen && (
           <div className="md:hidden mt-4">
             <ul className="flex flex-col items-center gap-4">
-              {NavbarMenu.map((menu) => (
-                <li
-                  key={menu.id}
-                  className="hover:text-slate-300 transition duration-300 ease-in-out transform hover:scale-105 text-xs py-2 px-3 uppercase"
-                >
-                  <a href={menu.link}>{menu.name}</a>
-                </li>
-              ))}
-              <button
-                className="hover:text-slate-300 transition duration-300 ease-in-out transform hover:scale-105 text-xl"
-              >
-                <Link to="/login">
-                  <MdAccountCircle />
-                </Link>
-              </button>
+              {children}
             </ul>
           </div>
         )}
